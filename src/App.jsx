fe6cb4666f3477750c9fb56f2b2472e6a379acf5
alegabo93 <alegabo93@hotmail.com>
2020-03-11 13:22:17 -0600

    fix: add sass and solve linter issues

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// Third party dependencies
+import React from 'react';
+
+// Own constants/actions/components
+import './App.scss';
+
+function App() {
+  return <h1>Resume Builder</h1>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+// Third party dependencies
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+
+// Own constants/actions/components
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Styles
+import './index.scss';
+
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
