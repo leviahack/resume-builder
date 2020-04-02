dadd76c9d9f407486ef08940ea2938f6d5c94a1f
alegabo93 <alegabo93@hotmail.com>
2020-04-02 13:24:47 -0600

    redux: add redux provider to app

--- src/App.test.js
+++ src/App.test.js
@@ -3,11 +3,13 @@ import React from 'react';
 import ReactTestRender from 'react-test-renderer';
 
 // Own constants/actions/components
+import configureStore from 'store/configure-store';
 import App from './App';
 
 describe('App: ', () => {
   test('should render correctly', () => {
-    const component = ReactTestRender.create(<App />);
+    const store = configureStore();
+    const component = ReactTestRender.create(<App store={store} />);
     const render = component.toJSON();
     expect(render).toMatchSnapshot();
   });
